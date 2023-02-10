58a5838ed23d45b639ebb8c3406855a62fac0034
Eunbeen Oh <svbean77@naver.com>
2023-02-10 20:33:41 +0900

    add link from react-router-dom

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -2,22 +2,31 @@ import React from "react";
 import LoginForm from "../components/LoginForm";
 import SocialLogin from "../components/SocialLogin";
 import "../style/Login.css";
+import { Link } from "react-router-dom";
 
 const Login = ({ LogoImg }) => {
   return (
     <>
-      <div className="logo">
-        <span className="logo-text">당근나라</span>
-        <img src={LogoImg} alt="당근나라번개공주" width={47} height={85} />
-        <span className="logo-text">번개공주</span>
-      </div>
+      <Link to={"/"}>
+        <div className="logo">
+          <span className="logo-text">당근나라</span>
+          <img src={LogoImg} alt="당근나라번개공주" width={47} height={85} />
+          <span className="logo-text">번개공주</span>
+        </div>
+      </Link>
       <LoginForm />
       <div className="search-info">
-        <span>아이디 찾기</span>
+        <Link to={"/searchID"}>
+          <span>아이디 찾기</span>
+        </Link>
         <span> | </span>
-        <span>비밀번호 찾기</span>
+        <Link to={"/searchPassword"}>
+          <span>비밀번호 찾기</span>
+        </Link>
         <span> | </span>
-        <span>회원가입</span>
+        <Link to={"/memberJoinDo"}>
+          <span>회원가입</span>
+        </Link>
       </div>
       <SocialLogin />
     </>
