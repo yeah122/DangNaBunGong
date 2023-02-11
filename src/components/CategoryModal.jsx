639c0357c20d9fec66c35abb836ae89eef8867bc
Eunbeen Oh <svbean77@naver.com>
2023-02-11 14:57:18 +0900

    category modal

--- src/components/CategoryModal.jsx
+++ src/components/CategoryModal.jsx
@@ -1,31 +1,44 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { categories } from "./CategoryLst";
+import "../style/Category.css";
 
 const CategoryModal = () => {
-  const [category, setCategory] = useState([]);
-  const [selected, setSelected] = useState();
-  // 최근 본 상품의 이미지, 링크 정보를 담음
-  const [recently, setRecently] = useState([
-    {
-      url: 0,
-      img: 0,
-    },
-    {
-      url: 1,
-      img: 1,
-    },
-  ]);
+  const [select, setSelect] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  const onMouseOver = () => {
+    setHover(true);
+  };
+
+  const onMouseOut = () => {
+    setHover(false);
+    setSelect(0);
+  };
 
   return (
-    <>
-      <div>카테고리</div>
-      <div>
-        <span>최근본상품</span>
-        {recently.map((item) => {
-          <span>item.img</span>;
-        })}
-      </div>
-    </>
+    <div
+      className="category-btn"
+      onMouseOver={onMouseOver}
+      onMouseOut={onMouseOut}
+    >
+      <button>카테고리</button>
+      {hover && (
+        <>
+          <div>
+            <ul>
+              {categories.map((item) => (
+                <Link to={`/${item.path}`}>
+                  <li key={item.id} onMouseOver={() => setSelect(item.id)}>
+                    {item.name}
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 
