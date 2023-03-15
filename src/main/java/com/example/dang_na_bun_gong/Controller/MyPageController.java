2fd7ee36699dff0f7e1333ce32f36dcf589bc330
JunHyLee <121077745+JunHyLee@users.noreply.github.com>
2023-03-15 15:41:19 +0900

    상대상세페이지 추가(미완성)

--- src/main/java/com/example/dang_na_bun_gong/Controller/MyPageController.java
+++ src/main/java/com/example/dang_na_bun_gong/Controller/MyPageController.java
@@ -31,13 +31,14 @@ public class MyPageController {
 
         String memberid = httpSession.getAttribute("memberid").toString(); // 세션에서 memberid 가져오기
 
+        List<MemberDto> myPageMemberInfo = myPageService.myPage_memberInfo(memberid);
         List<ArticleDto> myPageSell = myPageService.myPage_sellList(memberid);
         List<ArticleDto> myPageBuy = myPageService.myPage_buyList(memberid);
         List<ArticleDto> myPageLike = myPageService.myPage_likeList(memberid);
 
         JSONObject jsonObject = new JSONObject();
 
-        List<MemberDto> myPageMemberInfo = myPageService.myPage_memberInfo(memberid);
+
         List<MyPageMemberDto> myPageMemberDto = new ArrayList<>();
 
 
@@ -48,21 +49,21 @@ public class MyPageController {
 
         List<MyPageDto> myPageDto = new ArrayList<>();
 
-        for(int i = 0; i<myPageBuy.size(); i++){  //구매목록
+        for(int i = 0; i<5; i++){  //구매목록 (상위 5개)
             MyPageDto data = new MyPageDto(myPageBuy.get(i));
             myPageDto.add(data);
         }
         jsonObject.put("buyList", myPageDto);
         myPageDto.clear();
 
-        for(int i = 0; i<myPageSell.size(); i++){ //판매목록
+        for(int i = 0; i<5; i++){ //판매목록 상위(5개)
            MyPageDto data = new MyPageDto(myPageSell.get(i));
            myPageDto.add(data);
         }
         jsonObject.put("sellList", myPageDto);
         myPageDto.clear();
 
-        for(int i = 0; i<myPageLike.size(); i++){ //찜 목록
+        for(int i = 0; i<5; i++){ //찜 목록 상위(5개)
             MyPageDto data = new MyPageDto(myPageLike.get(i));
             myPageDto.add(data);
         }
@@ -82,6 +83,38 @@ public class MyPageController {
 
 
     //찜목록 전체보기
+
+    //상대상세페이지
+    @GetMapping("/opponentPage")
+    public @ResponseBody ResultVo opponentPage() {
+        String memberid = "";   // 상대방 회원 아이디
+
+        List<MemberDto> myPageMemberInfo = myPageService.myPage_memberInfo(memberid); //상대 회원정보
+        List<ArticleDto> myPageSell = myPageService.myPage_sellList(memberid); // 상대 판매목록
+
+        JSONObject jsonObject = new JSONObject();
+
+        List<MyPageMemberDto> myPageMemberDto = new ArrayList<>();
+
+        MyPageMemberDto data1 = new MyPageMemberDto(myPageMemberInfo.get(0)); // 프로필, 닉네임, 사진경로
+        myPageMemberDto.add(data1);
+        jsonObject.put("memberInfo", myPageMemberDto);
+        myPageMemberDto.clear();
+
+        List<MyPageDto> myPageDto = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) { //판매목록(상위 10개)
+            MyPageDto data = new MyPageDto(myPageSell.get(i));
+            myPageDto.add(data);
+        }
+        jsonObject.put("sellList", myPageDto);
+        myPageDto.clear();
+
+        //거래후기(아직 제작 안함)
+
+        return new ResultVo(0,"true", "success", jsonObject.toString());
+
+    }
 
 
 }
--- src/main/java/com/example/dang_na_bun_gong/Repository/MyPageRepository.java
+++ src/main/java/com/example/dang_na_bun_gong/Repository/MyPageRepository.java
@@ -14,13 +14,13 @@ import java.util.List;
 @Repository
 public interface MyPageRepository extends JpaRepository<BoardEntity, Integer>, JpaSpecificationExecutor<BoardEntity> {
 
-    @Query(value = "Select article_id, photo_fp From article where article.sell_member_id = :memberid Order by article_created DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "Select article_id, photo_fp From article where article.sell_member_id = :memberid Order by article_created DESC", nativeQuery = true)
     List<ArticleDto> sellList(String memberid);
 
     @Query(value = "Select article_id, photo_fp From article where article.sell_member_id = :memberid Order by article_created DESC", nativeQuery = true)
     List<ArticleDto> sellListAll(String memberid);
 
-    @Query(value = "Select article_id, photo_fp From article where article.buy_member_id = :memberid Order by article_created DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "Select article_id, photo_fp From article where article.buy_member_id = :memberid Order by article_created DESC", nativeQuery = true)
     List<ArticleDto> buyList(String memberid);
 
     @Query(value = "Select article_id, photo_fp From article where article.buy_member_id = :memberid Order by article_created DESC", nativeQuery = true)
@@ -29,10 +29,10 @@ public interface MyPageRepository extends JpaRepository<BoardEntity, Integer>, J
     @Query(value = "Select member_nickname, member_photo_fp, member_intro From member where member_id = :memberid", nativeQuery = true)
     List<MemberDto> memberInfo(String memberid);
 
-    @Query(value = "Select article.article_id, article.photo_fp From article join bookmark where bookmark.member_id = :memberid AND article.article_id = bookmark.article_id AND bookmark.like = 1 Order by article_created DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "Select article.article_id, article.photo_fp From article join bookmark where bookmark.member_id = :memberid AND article.article_id = bookmark.article_id AND bookmark.like = 1 Order by article_created DESC", nativeQuery = true)
     List<ArticleDto> likeList(String memberid);
 
-    @Query(value = "Select article.article_id, article.photo_fp From article join bookmark where bookmark.member_id = :memberid & article.article_id = bookmark.article_id  Order by article_created DESC LIMIT 5", nativeQuery = true)
+    @Query(value = "Select article.article_id, article.photo_fp From article join bookmark where bookmark.member_id = :memberid & article.article_id = bookmark.article_id  Order by article_created DESC", nativeQuery = true)
     List<ArticleDto> likeListAll(String memberid);
 
     //@Query(value = "Select member_nickname, member_photo_fp, member_intro From member where member_id = :memberid", nativeQuery = true)
